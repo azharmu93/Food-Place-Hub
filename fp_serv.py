44c97680311468517bf935220a75b30bf95e8e8d
andyhuang1117 <andy.huang@mail.utoronto.ca>
2015-03-02 21:46:32 -0500

    add oauth and push
    Andy added OAuth login and GCM PUSH message

--- fp_serv.py
+++ fp_serv.py
@@ -1,11 +1,59 @@
-from bottle import route, run, request, template, redirect, error, static_file, get, response
+from bottle import route, run, request, template, redirect, error, static_file, get, response, post
 import bottle
 import json
 import sqlite3
 
-#Add in more API's for the server
+###################OAuth Packages#######################
+from oauth2client.client import OAuth2WebServerFlow
+from oauth2client.client import flow_from_clientsecrets
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+import httplib2
+########################################################
 
-#Andy testing commit
+#####################GCM Functions######################
+from gcm import GCM
+gcm = GCM("AIzaSyDrBEVD-RkIKDNWGnt7S6C28tKDIgQk8Xg")
+
+device_token = {}
+
+@post('/registerAndroidDeviceForGCMPush')
+def registerAndroidDeviceForGCMPush():
+	global device_token
+	user_email = request.forms.get("user_email")
+	old_token = request.forms.get("old_push_device_token")
+	new_token = request.forms.get("new_push_device_token")
+	
+	if user_email not in device_token:
+		print "Adding an entry"
+		device_token[user_email] = new_token
+	elif user_email in device_token and device_token[user_email] != new_token:
+		print "Updating an entry"
+		device_token[user_email] = new_token
+
+	data = {}
+	data["status"] = "0"
+	print device_token
+	return json.dumps(data)
+
+@post('/sendTestPush')
+def sendTestPush():
+	global device_token
+	data = {'data': 'This is a test PUSH message'}
+	
+	# Plaintext request
+	#reg_id = 'XXXXXXXXXXXXX'
+	#gcm.plaintext_request(registration_id=reg_id, data=data)
+	
+	# JSON request
+	response = gcm.json_request(registration_ids=device_token.values(), data=data)
+
+	# Extra arguments
+	#res = gcm.json_request(registration_ids=reg_ids, data=data, collapse_key='uptoyou', delay_while_idle=True, time_to_live=3600)
+
+########################################################
+
+#Add in more API's for the server
 
 #Connect to the database
 conn = sqlite3.connect('restaurant.sqlite')
@@ -71,6 +119,45 @@ def reviewRating(id):
 	
 	return json.dumps(data)
 
-run(host = "localhost", port = 8080)
+######################OAuth Functions#########################
+@route('/signIn', 'GET')
+def signIn():
+	flow = flow_from_clientsecrets("client_secrets.json", scope="https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email", redirect_uri="http://just.test.com:8080/redirect")
+	uri = flow.step1_get_authorize_url()
+	redirect(str(uri))
 
+@route('/redirect')
+def redirect_page():
+	code = request.query.get('code', '')
+	flow = OAuth2WebServerFlow(client_id="868973567211-me9um9qovn4u9qqc4o624pscvgllqlac.apps.googleusercontent.com", client_secret="4J8V6p3pAzjFuo1JqQErIFd6", scope="https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email", redirect_uri="http://just.test.com:8080/redirect")
+	credentials = flow.step2_exchange(code)
+	token = credentials.id_token['sub']
+
+	print "Token: " + token
+	http = httplib2.Http()
+	http = credentials.authorize(http)
+
+	users_service = build('oauth2', 'v2', http=http)
+	user_document = users_service.userinfo().get().execute()
+	user_email = user_document['email']
+	user_name = user_document['name']
+	
+	print "User Name: " + user_name
+	print "User Email: " + user_email
+
+	url = "http://192.168.0.109:8080/authorized?user_name=" + user_name + "&user_email=" + user_email
+	redirect(url)
+
+@route('/authorized')
+def authorized():
+	name = request.query.getunicode("user_name", None, "utf8")
+	email = request.query.getunicode("user_email", None, "utf8")
+	data = {}
+	data["status"] = "0"
+	data["user_name"] = name
+	data["user_email"] = email
+	return json.dumps(data)
+##############################################################
+
+run(host = "192.168.0.109", port = 8080)
 conn.close()
